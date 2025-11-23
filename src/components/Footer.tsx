cfc49106a6b527a48ef8935a0c7339df7d17a0ec
Daniella Shemesh <daniellashemesh@MacBook-Air-sl-Daniella.local>
2025-11-23 11:36:02 +0200

    initial project setup

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ padding: "10px", marginTop: "40px", textAlign: "center", color: "#777" }}>
+      <p>© הקריה האקדמית אונו – המחלקה למדעי המחשב</p>
+    </footer>
+  );
+}
