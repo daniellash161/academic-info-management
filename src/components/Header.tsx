d09457923cb70c8c9f16e797c7c69f0416d601a2
ShaharKoza <kshahar1315@gmail.com>
2025-11-26 13:24:11 +0000

    הוספת header ועדכון app

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header style={{ padding: "20px", background: "#1e1e1e", color: "white" }}>
+      <h1>מערכת מידע למועמדים – הקריה האקדמית אונו</h1>
+    </header>
+  );
+}
+
+
